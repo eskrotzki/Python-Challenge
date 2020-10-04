9e5e6a589ad9d3c9f0913e9495547d85543a4e11
Erich <skrotzki808@gmail.com>
2020-10-04 15:27:17 -0600

    Update on Bank

---
+++ PyBank/main.py
@@ -0,0 +1,9 @@
+import os
+import csv
+
+budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+# Need to create to analyze Total months
+# Net total amount of "profit/losses" over entire period
+# Average of changes in P/L over entire period 
+# Greates increase and decrease (Date and Amount) over entire period
